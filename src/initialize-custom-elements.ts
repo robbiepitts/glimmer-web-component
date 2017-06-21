cea926e9be55577c2a5279f18f6eae3c238d11ce
Robbie Pitts <me@robbie-pitts.com>
2017-06-21 10:50:35 -0400

    Tweak API

--- src/initialize-custom-elements.ts
+++ src/initialize-custom-elements.ts
@@ -3,10 +3,10 @@ import glimmerElementFactory from './glimmer-element';
 
 export function initializeCustomElements(app: Application, customElementDefinitions: string[]): void {
   customElementDefinitions.forEach(name => {
-    initializeCustomElement(app, name);
+    initializeCustomElement(app, name, name);
   });
 }
 
-export function initializeCustomElement(app: Application, name: string): void {
-  window.customElements.define(name, glimmerElementFactory(app, name));
+export function initializeCustomElement(app: Application, componentName: string, elementName: string): void {
+  window.customElements.define(elementName, glimmerElementFactory(app, componentName));
 }
